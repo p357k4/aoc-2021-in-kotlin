8a4772b31b0354ba5660f997a2d2451a2b065514
Daniel Lewandowski <daniel.lewandowski@outlook.com>
2021-12-11 18:22:36 +0100

    updated Day10

--- src/Day10.kt
+++ src/Day10.kt
@@ -1,65 +1,65 @@
 fun main() {
-    fun analyze(input: String, stack: List<Char>): Int {
-        val result = when (val h = input.firstOrNull()) {
-            '(' -> analyze(input.drop(1), stack.plus(')'))
-            '[' -> analyze(input.drop(1), stack.plus(']'))
-            '{' -> analyze(input.drop(1), stack.plus('}'))
-            '<' -> analyze(input.drop(1), stack.plus('>'))
-            else -> if (stack.last() == h) {
-                analyze(input.drop(1), stack.dropLast(1))
-            } else {
-                when (h) {
-                    ')' -> 3
-                    ']' -> 57
-                    '}' -> 1197
-                    '>' -> 25137
-                    else -> 0
+    fun analyze(input: String): Int {
+        val stack = mutableListOf<Char>()
+        for (h in input) {
+            when (h) {
+                '(' -> stack.add(')')
+                '[' -> stack.add(']')
+                '{' -> stack.add('}')
+                '<' -> stack.add('>')
+                else -> if (stack.last() == h) {
+                    stack.removeLast()
+                } else {
+                    return when (h) {
+                        ')' -> 3
+                        ']' -> 57
+                        '}' -> 1197
+                        '>' -> 25137
+                        else -> 0
+                    }
                 }
             }
         }
 
-        return result
+        return 0
     }
 
-    fun analyze2(input: String, stack: List<Char>): Long {
-
-        val result = when (val h = input.firstOrNull()) {
-            '(' -> analyze2(input.drop(1), stack.plus(')'))
-            '[' -> analyze2(input.drop(1), stack.plus(']'))
-            '{' -> analyze2(input.drop(1), stack.plus('}'))
-            '<' -> analyze2(input.drop(1), stack.plus('>'))
-            else -> if (stack.last() == h) {
-                analyze2(input.drop(1), stack.dropLast(1))
-            } else {
-                when (h) {
-                    ')' -> 0
-                    ']' -> 0
-                    '}' -> 0
-                    '>' -> 0
-                    else ->
-                        stack.reversed().map {
-                        when (it) {
-                            ')' -> 1L
-                            ']' -> 2L
-                            '}' -> 3L
-                            '>' -> 4L
-                            else -> 0L
-                        }
-                    }.fold(0L) { acc, i -> 5 * acc + i }
+    fun analyze2(input: String): Long {
+        val stack = mutableListOf<Char>()
+        for (h in input) {
+            when (h) {
+                '(' -> stack.add(')')
+                '[' -> stack.add(']')
+                '{' -> stack.add('}')
+                '<' -> stack.add('>')
+                else -> if (stack.last() == h) {
+                    stack.removeLast()
+                } else {
+                    return 0
                 }
             }
         }
 
-        return result
+        return stack
+            .reversed()
+            .map {
+                when (it) {
+                    ')' -> 1L
+                    ']' -> 2L
+                    '}' -> 3L
+                    '>' -> 4L
+                    else -> 0L
+                }
+            }.fold(0L) { acc, i -> 5 * acc + i }
     }
 
     fun part1(input: List<String>): Int {
-        return input.sumOf { analyze(it, listOf()) }
+        return input.sumOf(::analyze)
     }
 
     fun part2(input: List<String>): Long {
         val result = input
-            .map { analyze2(it, listOf()) }
+            .map(::analyze2)
             .filter { it != 0L }
             .sorted()
         return result.get(result.size / 2)
