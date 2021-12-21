a1253856d1df286e32c797821504b590f6ea59a4
Daniel Lewandowski <daniel.lewandowski@outlook.com>
2021-12-21 22:43:19 +0100

    still no hope for Day19_notfinished.kt

--- src/Day19_notfinished.kt
+++ src/Day19_notfinished.kt
@@ -1,73 +1,69 @@
 fun main() {
-    data class Vector(val first: Int, val second: Int, val third: Int)
+    data class Vector(val x: Int, val y: Int, val z: Int)
 
     val transforms = arrayOf(
-        { v: Vector -> Vector(-v.first, -v.second, -v.third) },
-        { v: Vector -> Vector(-v.first, -v.third, -v.second) },
-        { v: Vector -> Vector(-v.second, -v.first, -v.third) },
-        { v: Vector -> Vector(-v.second, -v.third, -v.first) },
-        { v: Vector -> Vector(-v.third, -v.first, -v.second) },
-        { v: Vector -> Vector(-v.third, -v.second, -v.first) },
+        { v: Vector -> Vector(-v.x, -v.y, -v.z) },
+        { v: Vector -> Vector(-v.x, -v.z, -v.y) },
+        { v: Vector -> Vector(-v.y, -v.x, -v.z) },
+        { v: Vector -> Vector(-v.y, -v.z, -v.x) },
+        { v: Vector -> Vector(-v.z, -v.x, -v.y) },
+        { v: Vector -> Vector(-v.z, -v.y, -v.x) },
 
-        { v: Vector -> Vector(v.first, -v.second, -v.third) },
-        { v: Vector -> Vector(v.first, -v.third, -v.second) },
-        { v: Vector -> Vector(v.second, -v.first, -v.third) },
-        { v: Vector -> Vector(v.second, -v.third, -v.first) },
-        { v: Vector -> Vector(v.third, -v.first, -v.second) },
-        { v: Vector -> Vector(v.third, -v.second, -v.first) },
+        { v: Vector -> Vector(v.x, -v.y, -v.z) },
+        { v: Vector -> Vector(v.x, -v.z, -v.y) },
+        { v: Vector -> Vector(v.y, -v.x, -v.z) },
+        { v: Vector -> Vector(v.y, -v.z, -v.x) },
+        { v: Vector -> Vector(v.z, -v.x, -v.y) },
+        { v: Vector -> Vector(v.z, -v.y, -v.x) },
 
-        { v: Vector -> Vector(-v.first, v.second, -v.third) },
-        { v: Vector -> Vector(-v.first, v.third, -v.second) },
-        { v: Vector -> Vector(-v.second, v.first, -v.third) },
-        { v: Vector -> Vector(-v.second, v.third, -v.first) },
-        { v: Vector -> Vector(-v.third, v.first, -v.second) },
-        { v: Vector -> Vector(-v.third, v.second, -v.first) },
+        { v: Vector -> Vector(-v.x, v.y, -v.z) },
+        { v: Vector -> Vector(-v.x, v.z, -v.y) },
+        { v: Vector -> Vector(-v.y, v.x, -v.z) },
+        { v: Vector -> Vector(-v.y, v.z, -v.x) },
+        { v: Vector -> Vector(-v.z, v.x, -v.y) },
+        { v: Vector -> Vector(-v.z, v.y, -v.x) },
 
-        { v: Vector -> Vector(v.first, v.second, -v.third) },
-        { v: Vector -> Vector(v.first, v.third, -v.second) },
-        { v: Vector -> Vector(v.second, v.first, -v.third) },
-        { v: Vector -> Vector(v.second, v.third, -v.first) },
-        { v: Vector -> Vector(v.third, v.first, -v.second) },
-        { v: Vector -> Vector(v.third, v.second, -v.first) },
+        { v: Vector -> Vector(v.x, v.y, -v.z) },
+        { v: Vector -> Vector(v.x, v.z, -v.y) },
+        { v: Vector -> Vector(v.y, v.x, -v.z) },
+        { v: Vector -> Vector(v.y, v.z, -v.x) },
+        { v: Vector -> Vector(v.z, v.x, -v.y) },
+        { v: Vector -> Vector(v.z, v.y, -v.x) },
 
-        { v: Vector -> Vector(-v.first, -v.second, v.third) },
-        { v: Vector -> Vector(-v.first, -v.third, v.second) },
-        { v: Vector -> Vector(-v.second, -v.first, v.third) },
-        { v: Vector -> Vector(-v.second, -v.third, v.first) },
-        { v: Vector -> Vector(-v.third, -v.first, v.second) },
-        { v: Vector -> Vector(-v.third, -v.second, v.first) },
+        { v: Vector -> Vector(-v.x, -v.y, v.z) },
+        { v: Vector -> Vector(-v.x, -v.z, v.y) },
+        { v: Vector -> Vector(-v.y, -v.x, v.z) },
+        { v: Vector -> Vector(-v.y, -v.z, v.x) },
+        { v: Vector -> Vector(-v.z, -v.x, v.y) },
+        { v: Vector -> Vector(-v.z, -v.y, v.x) },
 
-        { v: Vector -> Vector(v.first, -v.second, v.third) },
-        { v: Vector -> Vector(v.first, -v.third, v.second) },
-        { v: Vector -> Vector(v.second, -v.first, v.third) },
-        { v: Vector -> Vector(v.second, -v.third, v.first) },
-        { v: Vector -> Vector(v.third, -v.first, v.second) },
-        { v: Vector -> Vector(v.third, -v.second, v.first) },
+        { v: Vector -> Vector(v.x, -v.y, v.z) },
+        { v: Vector -> Vector(v.x, -v.z, v.y) },
+        { v: Vector -> Vector(v.y, -v.x, v.z) },
+        { v: Vector -> Vector(v.y, -v.z, v.x) },
+        { v: Vector -> Vector(v.z, -v.x, v.y) },
+        { v: Vector -> Vector(v.z, -v.y, v.x) },
 
-        { v: Vector -> Vector(-v.first, v.second, v.third) },
-        { v: Vector -> Vector(-v.first, v.third, v.second) },
-        { v: Vector -> Vector(-v.second, v.first, v.third) },
-        { v: Vector -> Vector(-v.second, v.third, v.first) },
-        { v: Vector -> Vector(-v.third, v.first, v.second) },
-        { v: Vector -> Vector(-v.third, v.second, v.first) },
+        { v: Vector -> Vector(-v.x, v.y, v.z) },
+        { v: Vector -> Vector(-v.x, v.z, v.y) },
+        { v: Vector -> Vector(-v.y, v.x, v.z) },
+        { v: Vector -> Vector(-v.y, v.z, v.x) },
+        { v: Vector -> Vector(-v.z, v.x, v.y) },
+        { v: Vector -> Vector(-v.z, v.y, v.x) },
 
-        { v: Vector -> Vector(v.first, v.second, v.third) },
-        { v: Vector -> Vector(v.first, v.third, v.second) },
-        { v: Vector -> Vector(v.second, v.first, v.third) },
-        { v: Vector -> Vector(v.second, v.third, v.first) },
-        { v: Vector -> Vector(v.third, v.first, v.second) },
-        { v: Vector -> Vector(v.third, v.second, v.first) },
+        { v: Vector -> Vector(v.x, v.y, v.z) },
+        { v: Vector -> Vector(v.x, v.z, v.y) },
+        { v: Vector -> Vector(v.y, v.x, v.z) },
+        { v: Vector -> Vector(v.y, v.z, v.x) },
+        { v: Vector -> Vector(v.z, v.x, v.y) },
+        { v: Vector -> Vector(v.z, v.y, v.x) },
     )
 
-//    tailrec fun rotateX(v: Vector, i: Int): Vector = { if (i == 0) v else rotateX(Vector(v.first, -v.third, v.second), i - 1) }
-//    tailrec fun rotateY(v: Vector, i: Int): Vector = { if (i == 0) v else rotateY(Vector(v.third, v.second, -v.first), i -1) }
-//    tailrec fun rotateZ(v: Vector, i: Int): Vector = { if (i == 0) v else rotateZ(Vector(-v.second, v.first, v.third), i -1) }
-
-    fun read(input: List<String>): List<List<Vector>> {
-        val fold = input.fold(Pair(listOf<List<Vector>>(), listOf<Vector>())) { acc, line ->
+    fun read(input: Collection<String>): Collection<Set<Vector>> {
+        val fold = input.fold(Pair(listOf<Set<Vector>>(), setOf<Vector>())) { acc, line ->
             if (line == "") {
-                Pair(acc.first.plusElement(acc.second), listOf())
-            } else if (!line.startsWith("---")) {
+                Pair(acc.first.plusElement(acc.second), setOf())
+            } else if (!line.startsWith("--- ")) {
                 val split = line.split(',').map(String::toInt)
                 val triple = Vector(split[0], split[1], split[2])
                 Pair(acc.first, acc.second.plus(triple))
@@ -79,28 +75,22 @@ fun main() {
         return fold.first.plusElement(fold.second)
     }
 
-    fun sub(end: Vector, start: Vector) = Vector(
-        end.first - start.first,
-        end.second - start.second,
-        end.third - start.third
+    fun sub(start: Vector, end: Vector) = Vector(
+        end.x - start.x,
+        end.y - start.y,
+        end.z - start.z
     )
 
     fun add(start: Vector, end: Vector) = Vector(
-        end.first + start.first,
-        end.second + start.second,
-        end.third + start.third
+        end.x + start.x,
+        end.y + start.y,
+        end.z + start.z
     )
 
-    fun diffs(left: List<Vector>, right: List<Vector>): List<Vector> {
-        return left.flatMap { start ->
-            right.map { end ->
-                sub(end, start)
-            }
-        }
-    }
+    fun diffs(left: Collection<Vector>, right: Collection<Vector>) =
+        left.flatMap { start -> right.map { end -> sub(end, start) } }
 
-
-    fun part1(input: List<String>): Int {
+    fun part1(input: Collection<String>): Int {
         val cubes = read(input)
 
         val vectors = mutableListOf<Vector>()
@@ -112,34 +102,37 @@ fun main() {
 
         for (i in 0 until transformed.size - 1) {
             for (j in i + 1 until transformed.size) {
-                if (origins[j] != Vector(0,0,0)) {
+                if (origins[j] != Vector(0, 0, 0)) {
                     continue
                 }
                 val left = transformed[i]
                 val right = transformed[j]
                 val g = transforms
-                    .flatMap { transform ->
+                    .map { transform ->
                         val rightTransformed = right.map(transform)
-                        val d = diffs(rightTransformed, left)
-                                .groupBy { it }
-                                .mapValues { Pair(transform, it.value.size) }
-                        d.toList()
+                        val diffs = diffs(left, rightTransformed)
+
+                        val grouped = diffs.groupBy { it }
+                        val sized = grouped.mapValues { it.value.size }
+                        Pair(transform, sized)
                     }
+                    .filter { it.second.filter { it.value >= 12 }.isNotEmpty() }
 
-                val t = g
-                    .filter { it.second.second >= 12 }
-                    .maxByOrNull { it.second.second }
-
-                if (t != null) {
-                    origins[j] = add(origins[i], t.first)
-                    val f = transformed[j].map(t.second.first).map { add(it, origins[j]) }
-                    vectors.addAll(f)
-                    //transformed[j] = f
+                if (g.isEmpty()) {
+                    continue
                 }
+
+                val t = g.first()
+
+                val delta = t.second.filter { it.value >= 12 }.map { it.key }.first()
+                origins[j] = add(origins[i], delta)
+                val f = transformed[j].map(t.first).toSet()
+                vectors.addAll(f.map { add(it, origins[j]) })
+                transformed[j] = f
             }
         }
 
-        val g = vectors.toSet().sortedBy { it.first }
+        val g = vectors.toSet().sortedBy { it.x }
         return vectors.toSet().size
     }
 
